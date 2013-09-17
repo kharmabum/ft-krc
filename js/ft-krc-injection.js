a321952309e3086ea23e7dfac06a15d6cf5d1d5b
Juan-Carlos Foust <jc@fototropik.com>
2013-09-17 16:04:37 -0700

    remove vestigial code

--- js/ft-krc-injection.js
+++ js/ft-krc-injection.js
@@ -1,9 +1,12 @@
 $(function() {
-  if ($("#ft-krc-shell").length === 0) {
-    var url = chrome.extension.getURL("../ft-krc.html");
-    $("body").append("<div id=ft-krc-shell><iframe src='"+ url + "' height='100%' width='100%'></iframe></div>");
-    setTimeout(function(){$("#ft-krc-shell").show(300);},300);
-  } else {
-    $("#ft-krc-shell").toggle(300);
-  }
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if ($(".ft-krc-shell").length === 0) {
+          var url = chrome.extension.getURL("../ft-krc.html");
+          $("body").append("<div class='ft-krc-shell'><iframe src='"+ url + "' height='100%' width='100%'></iframe></div>");
+          $(".ft-krc-shell").show(300);
+        } else {
+          $(".ft-krc-shell").toggle(300);
+        }
+    });
 });
